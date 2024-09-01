520677040f2ea6ce091007df39428921aa6bf139
Josiah Kruse <sik645@gmail.com>
2024-09-01 16:52:15 -0700

    Implement vehicle list filtering in Vue app

--- var-spa-vue/src/components/VehicleList.vue
+++ var-spa-vue/src/components/VehicleList.vue
@@ -1,16 +1,42 @@
 <script setup lang="ts">
+import { ref, watch } from "vue";
 import { Vehicle } from "../interfaces/vehicle";
-import VehicleListItem from "./VehicleListItem.vue";
 import { useFetch } from "../composables/fetch";
+import VehicleListFilters from "./VehicleListFilters.vue";
+import VehicleListItem from "./VehicleListItem.vue";
+
+const filterText = ref<string>("");
+const unassignedOnly = ref<boolean>(false);
+
+function handleFilterTextChanged(newValue: string) {
+  filterText.value = newValue;
+}
+function handleUnassignedOnlyChanged(newValue: boolean) {
+  unassignedOnly.value = newValue;
+}
 
 const {data: vehicleList, error} = useFetch<Vehicle[]>("http://localhost:9000/assets");
+
+const vehicleListFiltered = ref<Vehicle[]>([]);
+watch([vehicleList, filterText, unassignedOnly], () => {
+  if (!vehicleList.value) return;
+  vehicleListFiltered.value = vehicleList.value
+    .filter(v => v.driver.includes(filterText.value))  // Apply text filter
+    .filter(v => unassignedOnly.value ? !v.driver : true);  // Apply "unassigned only" filter
+});
 </script>
 
 <template>
   <div v-if="error">Unable to fetch vehicle list. Error message: "{{ error.message }}"</div>
   <div v-else-if="vehicleList">
+    <VehicleListFilters
+      :filter-text="filterText"
+      :unassigned-only="unassignedOnly"
+      @filter-text-changed="handleFilterTextChanged"
+      @unassigned-only-changed="handleUnassignedOnlyChanged"
+    />
     <ul>
-      <li v-for="vehicle in vehicleList" :key="vehicle.id">
+      <li v-for="vehicle in vehicleListFiltered" :key="vehicle.id">
         <VehicleListItem :vehicle />
       </li>
     </ul>
---
+++ var-spa-vue/src/components/VehicleListFilters.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const props = defineProps<{
+  filterText: string;
+  unassignedOnly: boolean;
+}>();
+
+const emit = defineEmits<{
+  filterTextChanged: [newValue: string];
+  unassignedOnlyChanged: [newValue: boolean];
+}>();
+
+function handleFilterTextInput(e: any) {
+  emit('filterTextChanged', e.target.value);
+}
+function handleUnassignedOnlyChange(e: any) {
+  emit('unassignedOnlyChanged', e.target.checked);
+}
+</script>
+
+<template>
+  <form>
+    <label>
+      Driver
+      <input
+        type="text"
+        :value="props.filterText"
+        @input="handleFilterTextInput"
+      />
+    </label>
+    <label>
+      Unassigned
+      <input
+        type="checkbox"
+        :checked="props.unassignedOnly"
+        @change="handleUnassignedOnlyChange"
+      />
+    </label>
+  </form>
+</template>
